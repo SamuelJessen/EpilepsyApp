8d3439f51a6ba12c5b02717acb5a85a6d1999d74
SamuelJessen <201906916@post.au.dk>
2024-05-06 11:14:42 +0200

    Added login. Uncommented for now, for test purporses

--- EpilepsyApp/ViewModel/MainViewModel.cs
+++ EpilepsyApp/ViewModel/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using EpilepsyApp.Services;
 
@@ -16,6 +19,7 @@ namespace EpilepsyApp.ViewModel
 		private readonly IDecoder decoder;
 		public IMQTTService mqttService { get; set; }
 		public IRawDataService rawDataService { get; set; }
+		private readonly HttpClient httpClient;
 
 		public MainViewModel(BLEservice ble, IDecoder decoder, IMQTTService mqttClient, IRawDataService rawDataClient)
 		{
@@ -23,12 +27,29 @@ namespace EpilepsyApp.ViewModel
 			this.decoder = decoder;
 			mqttService = mqttClient;
 			rawDataService = rawDataClient;
+			httpClient = new HttpClient();
 		}
 
 		[ICommand]
-		async Task Login()
+		public async Task Login()
 		{
 			await Shell.Current.GoToAsync($"{nameof(MonitoringPage)}?Username={username}");
+
+			//var loginRequest = new { Id = username, Password = password };
+			//var json = JsonSerializer.Serialize(loginRequest);
+			//var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+			//HttpResponseMessage response = await httpClient.PostAsync("https://localhost:7128/patients/login", content);
+
+			//if (response.IsSuccessStatusCode)
+			//{
+			//	await Shell.Current.GoToAsync($"{nameof(MonitoringPage)}?Username={username}");
+			//}
+			//else
+			//{
+			//	string errorMessage = $"Login failed: {response.StatusCode} - {response.ReasonPhrase}";
+			//	await Shell.Current.DisplayAlert("Login Failed", errorMessage, "OK");
+			//}
 		}
 
 	}
